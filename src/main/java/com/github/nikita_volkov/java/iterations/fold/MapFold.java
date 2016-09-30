7468df15a7204f99319c05ff11071bfd54b6519e
Nikita Volkov <nikita.y.volkov@mail.ru>
2016-09-30 08:54:36 +0300

    MapFold, ZipFold

---
+++ src/main/java/com/github/nikita_volkov/java/iterations/fold/MapFold.java
@@ -0,0 +1,30 @@
+package com.github.nikita_volkov.java.iterations.fold;
+
+import java.util.function.Function;
+
+public final class MapFold<input, accumulator, output1, output2> implements Fold<input, accumulator, output2> {
+
+  private final Fold<input, accumulator, output1> initialFold;
+  private final Function<output1, output2> projection;
+
+  public MapFold(Fold<input, accumulator, output1> initialFold, Function<output1, output2> projection) {
+    this.initialFold = initialFold;
+    this.projection = projection;
+  }
+
+  @Override
+  public accumulator init() {
+    return initialFold.init();
+  }
+
+  @Override
+  public accumulator step(accumulator accumulator, input input) {
+    return initialFold.step(accumulator, input);
+  }
+
+  @Override
+  public output2 extract(accumulator accumulator) {
+    return projection.apply(initialFold.extract(accumulator));
+  }
+
+}
