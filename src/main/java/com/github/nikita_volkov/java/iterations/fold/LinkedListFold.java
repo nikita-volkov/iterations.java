28c338029e295f12aef02080834ce39d8cea9cbd
Nikita Volkov <nikita.y.volkov@mail.ru>
2016-09-30 08:54:51 +0300

    LinkedListFold

---
+++ src/main/java/com/github/nikita_volkov/java/iterations/fold/LinkedListFold.java
@@ -0,0 +1,21 @@
+package com.github.nikita_volkov.java.iterations.fold;
+
+import java.util.*;
+
+public final class LinkedListFold<element> implements Fold<element, LinkedList<element>, LinkedList<element>> {
+  @Override
+  public LinkedList<element> init() {
+    return new LinkedList<element>();
+  }
+
+  @Override
+  public LinkedList<element> step(LinkedList<element> accumulator, element input) {
+    accumulator.add(input);
+    return accumulator;
+  }
+
+  @Override
+  public LinkedList<element> extract(LinkedList<element> accumulator) {
+    return accumulator;
+  }
+}
