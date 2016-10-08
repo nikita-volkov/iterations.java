83863d156bab2ef852cc70dd7c47a3578f4f0c91
Nikita Volkov <nikita.y.volkov@mail.ru>
2016-10-08 12:53:43 +0300

    Use composite types

--- src/main/java/com/github/nikita_volkov/java/iterations/ParIteration.java
+++ src/main/java/com/github/nikita_volkov/java/iterations/ParIteration.java
@@ -1,6 +1,8 @@
 package com.github.nikita_volkov.java.iterations;
 
-public final class ParIteration<input1, input2, output1, output2> implements Iteration<ParIteration.Pair<input1, input2>, ParIteration.Pair<output1, output2>> {
+import com.github.nikita_volkov.java.composites.Product2;
+
+public final class ParIteration<input1, input2, output1, output2> implements Iteration<Product2<input1, input2>, Product2<output1, output2>> {
 
   private final Iteration<input1, output1> iteration1;
   private final Iteration<input2, output2> iteration2;
@@ -22,7 +24,7 @@ public final class ParIteration<input1, input2, output1, output2> implements Ite
   }
 
   @Override
-  public boolean step(Pair<input1, input2> input) {
+  public boolean step(Product2<input1, input2> input) {
     if (iteration1Active) {
       iteration1Active = iteration1.step(input._1);
     }
@@ -33,18 +35,8 @@ public final class ParIteration<input1, input2, output1, output2> implements Ite
   }
 
   @Override
-  public Pair<output1, output2> output() {
-    return new Pair<>(iteration1.output(), iteration2.output());
-  }
-
-  public final static class Pair<_1, _2> {
-    public final _1 _1;
-    public final _2 _2;
-
-    public Pair(_1 _1, _2 _2) {
-      this._1 = _1;
-      this._2 = _2;
-    }
+  public Product2<output1, output2> output() {
+    return new Product2<>(iteration1.output(), iteration2.output());
   }
 
 }
--- src/main/java/com/github/nikita_volkov/java/iterations/ZipIteration.java
+++ src/main/java/com/github/nikita_volkov/java/iterations/ZipIteration.java
@@ -1,6 +1,8 @@
 package com.github.nikita_volkov.java.iterations;
 
-public final class ZipIteration<input, output1, output2> implements Iteration<input, ZipIteration.Pair<output1, output2>> {
+import com.github.nikita_volkov.java.composites.Product2;
+
+public final class ZipIteration<input, output1, output2> implements Iteration<input, Product2<output1, output2>> {
 
   private final Iteration<input, output1> iteration1;
   private final Iteration<input, output2> iteration2;
@@ -33,18 +35,8 @@ public final class ZipIteration<input, output1, output2> implements Iteration<in
   }
 
   @Override
-  public Pair<output1, output2> output() {
-    return new Pair<>(iteration1.output(), iteration2.output());
-  }
-
-  public final static class Pair<_1, _2> {
-    public final _1 _1;
-    public final _2 _2;
-
-    public Pair(_1 _1, _2 _2) {
-      this._1 = _1;
-      this._2 = _2;
-    }
+  public Product2<output1, output2> output() {
+    return new Product2<>(iteration1.output(), iteration2.output());
   }
 
 }
